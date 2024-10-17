45de46037350485cba1700a7da52724e69a3d5d8
laura147145 <aristizaballaura850@gmail.com>
2024-10-17 16:13:08 -0500

    se crea y se valida que funciona el inicio de secion

---
+++ script.js
@@ -0,0 +1,9 @@
+let Usuario = "hola"
+let Contrasña = "hola123"
+let inputUsurio = prompt("Ingrese su Usurio")
+let inputContraseña = prompt("Ingrese su contraseña")
+if(Usuario == inputUsurio && Contrasña == inputContraseña){
+    console.log("Bienvenido" + inputUsurio)
+}else{
+    console.log("Usuario y/o Contraseña Incorrecta")
+}
